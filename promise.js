77be1da7d1f6b410037bad1903b0b6eeb9f8e69a
Yoga Ari Tofan <y.aritofan@gmail.com>
2022-06-14 14:37:11 +0700

    challenge: JS Promise

--- promise.js
+++ promise.js
@@ -1,7 +1,17 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (emotion) => {
+  let xxi = await promiseTheaterIXX();
+  let cgv = await promiseTheaterVGC();
+
+  let cinemas = xxi.concat(cgv);
+  let results = 0;
+
+  cinemas.map((xxi) => xxi.hasil === emotion && (results += 1));
+
+  return results;
+};
 
 module.exports = {
   promiseOutput,
